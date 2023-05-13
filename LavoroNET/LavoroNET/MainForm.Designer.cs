bda66088663749d06327e5a9024db62869a9292b
Mirzakhmet Syzdykov <mspmail598@gmail.com>
2023-05-14 01:03:25 +0600

    Final release
    Version 1.0.0

--- LavoroNET/LavoroNET/MainForm.Designer.cs
+++ LavoroNET/LavoroNET/MainForm.Designer.cs
@@ -14,6 +14,17 @@ namespace LavoroNET
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TabControl tabControlMain;
+		private System.Windows.Forms.TabPage tabPageFileBrowser;
+		private System.Windows.Forms.WebBrowser webBrowserFile;
+		private System.Windows.Forms.TabPage tabPagePreview;
+		private System.Windows.Forms.WebBrowser webBrowserPreview;
+		private System.Windows.Forms.Label labelPreviewTitle;
+		private System.Windows.Forms.TextBox textBoxPreviewTitle;
+		private System.Windows.Forms.Label labelNavigate;
+		private System.Windows.Forms.TextBox textBoxNavigate;
+		private System.Windows.Forms.Button buttonNavigate;
+		private System.Windows.Forms.Button buttonAddFile;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -36,15 +47,143 @@ namespace LavoroNET
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.tabControlMain = new System.Windows.Forms.TabControl();
+			this.tabPageFileBrowser = new System.Windows.Forms.TabPage();
+			this.webBrowserFile = new System.Windows.Forms.WebBrowser();
+			this.tabPagePreview = new System.Windows.Forms.TabPage();
+			this.buttonAddFile = new System.Windows.Forms.Button();
+			this.buttonNavigate = new System.Windows.Forms.Button();
+			this.textBoxNavigate = new System.Windows.Forms.TextBox();
+			this.labelNavigate = new System.Windows.Forms.Label();
+			this.textBoxPreviewTitle = new System.Windows.Forms.TextBox();
+			this.webBrowserPreview = new System.Windows.Forms.WebBrowser();
+			this.labelPreviewTitle = new System.Windows.Forms.Label();
+			this.tabControlMain.SuspendLayout();
+			this.tabPageFileBrowser.SuspendLayout();
+			this.tabPagePreview.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// tabControlMain
+			// 
+			this.tabControlMain.Controls.Add(this.tabPageFileBrowser);
+			this.tabControlMain.Controls.Add(this.tabPagePreview);
+			this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+			this.tabControlMain.Name = "tabControlMain";
+			this.tabControlMain.SelectedIndex = 0;
+			this.tabControlMain.Size = new System.Drawing.Size(758, 430);
+			this.tabControlMain.TabIndex = 0;
+			// 
+			// tabPageFileBrowser
+			// 
+			this.tabPageFileBrowser.Controls.Add(this.webBrowserFile);
+			this.tabPageFileBrowser.Location = new System.Drawing.Point(4, 25);
+			this.tabPageFileBrowser.Name = "tabPageFileBrowser";
+			this.tabPageFileBrowser.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageFileBrowser.Size = new System.Drawing.Size(750, 401);
+			this.tabPageFileBrowser.TabIndex = 1;
+			this.tabPageFileBrowser.Text = "File Browser";
+			this.tabPageFileBrowser.UseVisualStyleBackColor = true;
+			// 
+			// webBrowserFile
+			// 
+			this.webBrowserFile.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowserFile.Location = new System.Drawing.Point(3, 3);
+			this.webBrowserFile.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowserFile.Name = "webBrowserFile";
+			this.webBrowserFile.Size = new System.Drawing.Size(744, 395);
+			this.webBrowserFile.TabIndex = 0;
+			// 
+			// tabPagePreview
+			// 
+			this.tabPagePreview.Controls.Add(this.buttonAddFile);
+			this.tabPagePreview.Controls.Add(this.buttonNavigate);
+			this.tabPagePreview.Controls.Add(this.textBoxNavigate);
+			this.tabPagePreview.Controls.Add(this.labelNavigate);
+			this.tabPagePreview.Controls.Add(this.textBoxPreviewTitle);
+			this.tabPagePreview.Controls.Add(this.webBrowserPreview);
+			this.tabPagePreview.Controls.Add(this.labelPreviewTitle);
+			this.tabPagePreview.Location = new System.Drawing.Point(4, 25);
+			this.tabPagePreview.Name = "tabPagePreview";
+			this.tabPagePreview.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPagePreview.Size = new System.Drawing.Size(750, 401);
+			this.tabPagePreview.TabIndex = 2;
+			this.tabPagePreview.Text = "Preview";
+			// 
+			// buttonAddFile
+			// 
+			this.buttonAddFile.Location = new System.Drawing.Point(628, 57);
+			this.buttonAddFile.Name = "buttonAddFile";
+			this.buttonAddFile.Size = new System.Drawing.Size(79, 23);
+			this.buttonAddFile.TabIndex = 6;
+			this.buttonAddFile.Text = "Add file";
+			this.buttonAddFile.UseVisualStyleBackColor = true;
+			this.buttonAddFile.Click += new System.EventHandler(this.ButtonAddFileClick);
+			// 
+			// buttonNavigate
+			// 
+			this.buttonNavigate.Location = new System.Drawing.Point(628, 20);
+			this.buttonNavigate.Name = "buttonNavigate";
+			this.buttonNavigate.Size = new System.Drawing.Size(79, 23);
+			this.buttonNavigate.TabIndex = 5;
+			this.buttonNavigate.Text = "Navigate";
+			this.buttonNavigate.UseVisualStyleBackColor = true;
+			this.buttonNavigate.Click += new System.EventHandler(this.ButtonNavigateClick);
+			// 
+			// textBoxNavigate
+			// 
+			this.textBoxNavigate.Location = new System.Drawing.Point(97, 20);
+			this.textBoxNavigate.Name = "textBoxNavigate";
+			this.textBoxNavigate.Size = new System.Drawing.Size(516, 22);
+			this.textBoxNavigate.TabIndex = 4;
+			// 
+			// labelNavigate
+			// 
+			this.labelNavigate.Location = new System.Drawing.Point(16, 20);
+			this.labelNavigate.Name = "labelNavigate";
+			this.labelNavigate.Size = new System.Drawing.Size(70, 23);
+			this.labelNavigate.TabIndex = 3;
+			this.labelNavigate.Text = "Navigate:";
+			// 
+			// textBoxPreviewTitle
+			// 
+			this.textBoxPreviewTitle.Location = new System.Drawing.Point(95, 57);
+			this.textBoxPreviewTitle.Name = "textBoxPreviewTitle";
+			this.textBoxPreviewTitle.ReadOnly = true;
+			this.textBoxPreviewTitle.Size = new System.Drawing.Size(518, 22);
+			this.textBoxPreviewTitle.TabIndex = 2;
+			// 
+			// webBrowserPreview
+			// 
+			this.webBrowserPreview.Location = new System.Drawing.Point(0, 108);
+			this.webBrowserPreview.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowserPreview.Name = "webBrowserPreview";
+			this.webBrowserPreview.Size = new System.Drawing.Size(747, 293);
+			this.webBrowserPreview.TabIndex = 1;
+			this.webBrowserPreview.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowserPreviewDocumentCompleted);
+			this.webBrowserPreview.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.WebBrowserPreviewNavigated);
+			// 
+			// labelPreviewTitle
+			// 
+			this.labelPreviewTitle.Location = new System.Drawing.Point(16, 57);
+			this.labelPreviewTitle.Name = "labelPreviewTitle";
+			this.labelPreviewTitle.Size = new System.Drawing.Size(49, 23);
+			this.labelPreviewTitle.TabIndex = 0;
+			this.labelPreviewTitle.Text = "Title:";
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(762, 426);
+			this.Controls.Add(this.tabControlMain);
 			this.Name = "MainForm";
 			this.Text = "LavoroNET";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.Resize += new System.EventHandler(this.MainFormResize);
+			this.tabControlMain.ResumeLayout(false);
+			this.tabPageFileBrowser.ResumeLayout(false);
+			this.tabPagePreview.ResumeLayout(false);
+			this.tabPagePreview.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
--- LavoroNET/LavoroNET/MainForm.cs
+++ LavoroNET/LavoroNET/MainForm.cs
@@ -10,12 +10,16 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Security.Permissions;
+using System.IO;
+using System.Text;
 
 namespace LavoroNET
 {
 	/// <summary>
-	/// Description of MainForm.
+	/// The web OS main window.
 	/// </summary>
+	[System.Runtime.InteropServices.ComVisibleAttribute(true)]
 	public partial class MainForm : Form
 	{
 		public MainForm()
@@ -28,6 +32,102 @@ namespace LavoroNET
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
+		}
+		
+		public const string FilesPath = @"C:\Users\mspma\Documents\GitHub\Lavoro\LavoroCore\public_html\files.js";
+		
+		public const string BrowserPath = @"C:\Users\mspma\Documents\GitHub\Lavoro\LavoroCore\public_html\browser.html";
+		
+		public void UpdateFiles(String s) {
+			File.WriteAllText(FilesPath, "var files = [" + s + "];", Encoding.UTF8);
+			
+			this.webBrowserFile.Navigate(new Uri(BrowserPath));	
+		}
+		
+		public void PreviewFile(String url, String cookie) {
+			this.webBrowserPreview.Url = new Uri(url);
+			
+			this.webBrowserPreview.Document.Cookie = cookie;
+			
+			this.webBrowserPreview.Navigate(new Uri(url));
+
+			this.tabControlMain.SelectedTab = this.tabPagePreview;
+		}
+		
+		public void AddFile(String url, String cookie) {
+			this.webBrowserFile.Document.InvokeScript("addFileNET", new string[] { url, cookie });
+			
+			this.webBrowserFile.Document.InvokeScript("updateFilesNET");
+			
+			this.webBrowserFile.Navigate(new Uri(BrowserPath));
+		}
+		
+		public void AddFileFromInput() {
+			this.webBrowserFile.Document.InvokeScript("updateFilesNET");
+
+			this.webBrowserFile.Navigate(new Uri(BrowserPath));			
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			this.webBrowserFile.ScriptErrorsSuppressed = true;
+			
+			this.webBrowserPreview.ScriptErrorsSuppressed = true;
+			
+			this.webBrowserFile.ObjectForScripting = this.webBrowserPreview.ObjectForScripting = this;
+			
+			this.webBrowserFile.Url = new Uri(BrowserPath);
+			
+			this.MainFormResize(this, null);
+		}
+		
+		void ButtonNavigateClick(object sender, EventArgs e)
+		{
+			this.webBrowserPreview.Url = new Uri(this.textBoxNavigate.Text);
+		}
+		
+		void WebBrowserPreviewDocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			this.textBoxPreviewTitle.Text = this.webBrowserPreview.DocumentTitle;
+		}
+		
+		void ButtonAddFileClick(object sender, EventArgs e)
+		{
+			this.webBrowserFile.Document.InvokeScript("addFileNET", new string[] { this.webBrowserPreview.Url.AbsoluteUri.ToString(), this.webBrowserPreview.Document.Cookie != null ? this.webBrowserPreview.Document.Cookie.ToString() : "" });
+			
+			this.webBrowserFile.Document.InvokeScript("updateFilesNET");
+			
+			this.webBrowserFile.Navigate(new Uri(BrowserPath));
+		}
+		
+		void WebBrowserPreviewNavigated(object sender, WebBrowserNavigatedEventArgs e)
+		{
+			this.textBoxNavigate.Text = this.webBrowserPreview.Url.AbsoluteUri;
+			
+			this.textBoxPreviewTitle.Text = this.webBrowserPreview.DocumentTitle;
+		}
+		
+		void MainFormResize(object sender, EventArgs e)
+		{
+			this.tabControlMain.Width = this.Width;
+			
+			this.tabControlMain.Height = this.Height;
+			
+			this.tabPageFileBrowser.Width = this.tabControlMain.Width;
+
+			this.tabPageFileBrowser.Height = this.tabControlMain.Height;
+			
+			this.webBrowserFile.Width = this.tabPageFileBrowser.Width;
+			
+			this.webBrowserFile.Height = this.tabPageFileBrowser.Height;
+			
+			this.tabPagePreview.Width = this.tabControlMain.Width;
+			
+			this.tabPagePreview.Height = this.tabControlMain.Height;
+			
+			this.webBrowserPreview.Width = this.tabPagePreview.Width;
+			
+			this.webBrowserPreview.Height = this.tabPagePreview.Height - 108;
 		}
 	}
 }
